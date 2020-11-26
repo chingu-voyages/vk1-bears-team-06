bf0a5b6be366ad757ea79338dc3e346d1973c833
Sam Norton <samuelnico.norton@gmail.com>
2020-11-26 14:50:41 +0800

    Setup nodemon and concurrently to run only one script for client and server side

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const resorts = require('./data/resorts')
+
+dotenv.config()
+
 
 const app = express()
 
@@ -17,4 +21,7 @@ app.get('/api/resorts/:id', (req, res) => {
 })
 
 
-app.listen(5000, console.log('Server running on port 5000')) 
+const PORT = process.env.PORT || 5000
+const MODE = process.env.NODE_ENV
+
+app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`)) 
