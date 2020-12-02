a5bf0df397a8375cb122cdd0a7a467c5597d703e
Sam Norton <samuelnico.norton@gmail.com>
2020-12-02 15:06:22 +0800

    Transferred the routes functionalities to controllers

---
+++ backend/controllers/resortController.js
@@ -0,0 +1,27 @@
+import expressAsyncHandler from 'express-async-handler'
+import Resort from '../models/resortModel.js'
+
+// @description   Fetch all resorts
+// @route         GET /api/resorts
+// @access        Public
+const getResorts = expressAsyncHandler(async (req, res) => {
+    const resorts = await Resort.find({})
+    res.json(resorts)
+})
+
+
+// @description   Fetch a single resort
+// @route         GET /api/resorts/:id
+// @access        Public
+const getResortById = expressAsyncHandler(async (req, res) => {
+    const resort = await Resort.findById(req.params.id)
+    
+    if(resort){ 
+        res.json(resort) 
+    } else {
+        res.status(404)
+        throw new Error('Resort not found')
+    }
+})
+
+export { getResorts, getResortById } 
--- backend/routes/resortRoutes.js
+++ backend/routes/resortRoutes.js
@@ -1,31 +1,8 @@
 import express from 'express'
-import expressAsyncHandler from 'express-async-handler'
-
+import { getResorts, getResortById } from '../controllers/resortController.js'
 const router = express.Router()
 
-import Resort from '../models/resortModel.js'
-
-// @description   Fetch all resorts
-// @route         GET /api/resorts
-// @access        Public
-router.get('/', expressAsyncHandler (async (req, res) => {
-    const resorts = await Resort.find({})
-    res.json(resorts)
-}))
-
-
-// @description   Fetch a single resort
-// @route         GET /api/resorts/:id
-// @access        Public
-router.get('/:id', expressAsyncHandler (async (req, res) => {
-    const resort = await Resort.findById(req.params.id)
-    
-    if(resort){ 
-        res.json(resort) 
-    } else {
-        res.status(404)
-        throw new Error('Resort not found')
-    }
-}))
+router.route('/').get(getResorts)
+router.route('/:id').get(getResortById)
 
 export default router
