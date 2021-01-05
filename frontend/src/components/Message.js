54d6147f01665f2fa1241fa27879c02a386fe90d
nocosharold <nocosharold@gmail.com>
2021-01-06 00:02:03 +0800

    Added markup on user-list-screen

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert } from 'react-bootstrap'
 
 const Message = ({ variant, children }) => {
-    return (
-        <>
-            <div className={`alert-${variant}`} role="alert">
-                 {children}
-            </div>
-        </>
-    )
+    const [show, setShow] = useState(true);
+
+    if(show) {
+        return (
+            <>
+                <Alert className={`alert alert-${variant}`} onClose={() => setShow(false)} dismissible role="alert">
+                    <p>{children}</p>
+                </Alert>
+            </>
+        )
+    } else {
+        return () => setShow(true)
+    }
 }
 
 Message.defaultProps = { 
