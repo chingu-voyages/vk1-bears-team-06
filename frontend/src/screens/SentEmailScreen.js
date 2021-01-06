d666d0d6b18a2a331069ada17bc99468f79c3d0f
Sam Norton <samuelnico.norton@gmail.com>
2021-01-06 09:20:25 +0800

    Fixed sent confirmation email screen

--- frontend/src/screens/SentEmailScreen.js
+++ frontend/src/screens/SentEmailScreen.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import  emailSent from '../assets/images/svg/email-sent.svg'
 
 const SentEmailScreen = ({ match }) => {
-     
     const email = match.params.email 
-
     return (
         <>
-            <h1>We've sent an email to {email}</h1>
-            <p>Please check and confirm</p>
+        <div className="confirmation">
+            <div className="overlay-img"></div>
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-xs-12 join">
+                        <div className="confirm-email">
+                            <img src={emailSent} alt="mail" />
+                            <h2>Verify Your Email Address</h2>
+                            <p>We have sent you a link to the email below. Click the link to confirm your account. If you can't find the email, check your spam folder.</p>
+                            <Link to="/" className="user-email">{email}</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
