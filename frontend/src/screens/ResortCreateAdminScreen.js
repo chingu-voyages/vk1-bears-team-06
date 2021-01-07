6cd1293735716250731ae25b8b68af730e75ed9e
nocosharold <nocosharold@gmail.com>
2021-01-07 10:03:18 +0800

    Fixed resort-create-admin-screen Website Validation

--- frontend/src/screens/ResortCreateAdminScreen.js
+++ frontend/src/screens/ResortCreateAdminScreen.js
@@ -271,7 +271,7 @@ const ResortCreateAdminScreen = ({ history }) => {
                                             name="website" 
                                             className={`form-control ${errors.website ? 'is-invalid' : ''}`}
                                             id="website" 
-                                            ref={register({ required: true, pattern: /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/ })}
+                                            ref={register({ required: true, pattern: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ })}
                                         />
                                         { errors.website && errors.website.type === 'required' && <p className="text-danger">Website is required.</p>}
                                         { errors.website && errors.website.type ==='pattern' && <p className="text-danger">Not a valid website url.</p> }
