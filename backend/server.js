03d0caa81663f158968b8de10b6b4925b1354f92
Sam Norton <samuelnico.norton@gmail.com>
2021-01-07 21:00:30 +0800

    Prepare files for deployment

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,20 @@ app.use('/api/resortsByOwner', resortByOwnerRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/upload', uploadRoutes)
 
+
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
+
 
 app.use(notFound)
 
